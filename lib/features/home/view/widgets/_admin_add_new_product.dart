eb7956416be6f1b6bfcf9faf935a3f33933f958f
Kaium Al Limon <limon.3cs@gmail.com>
2024-08-20 13:46:31 +0600

    Add new product update

--- lib/features/home/view/widgets/_admin_add_new_product.dart
+++ lib/features/home/view/widgets/_admin_add_new_product.dart
@@ -1,128 +1,289 @@
-import 'package:attira/features/home/controller/_admin_add_product_provider.dart';
+import 'dart:io';
+import 'package:attira/features/register/view/widgets/_customDialog.dart';
 import 'package:attira/services/products/firebase/_product_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
 
-class AddNewProduct extends ConsumerWidget {
-  AddNewProduct({super.key});
-
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _priceController = TextEditingController();
+class AddNewProduct extends ConsumerStatefulWidget {
+  const AddNewProduct({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final theme = Theme.of(context).colorScheme;
-    final productService = FirebaseProductService();
+  _AddNewProductState createState() => _AddNewProductState();
+}
 
-    final adminAddProduct = ref.watch(adminAddProductProvider);
-    final adminAddProductNotifier = ref.read(adminAddProductProvider.notifier);
+class _AddNewProductState extends ConsumerState<AddNewProduct> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _priceController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _categoryController = TextEditingController();
+  final TextEditingController _sizesController = TextEditingController();
+  final TextEditingController _colorsController = TextEditingController();
+  final TextEditingController _tagsController = TextEditingController();
+  final List<XFile> _imageFiles = []; // Change to List
+
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _pickImages() async {
+    final List<XFile>? images =
+        await _picker.pickMultiImage(); // Use pickMultiImage
+    if (images != null) {
+      setState(() {
+        _imageFiles.addAll(images);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add New Product'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            CustomButton(
-              width: double.infinity,
-              height: 200,
-              primaryColor: theme.primary.withOpacity(.1),
-              textColor: theme.onPrimary,
-              onPressed: () {},
-              textBold: true,
-              textSize: 12,
-              borderRadius: 13,
-              label: 'Add',
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: ListView(
                   children: [
-                    Icon(
-                      Icons.add,
-                      color: theme.onSurface.withOpacity(.5),
-                      size: 25,
+                    GestureDetector(
+                      onTap: _pickImages,
+                      child: CustomButton(
+                        width: double.infinity,
+                        height: 200,
+                        primaryColor: theme.primary.withOpacity(.1),
+                        textColor: theme.onPrimary,
+                        onPressed: _pickImages,
+                        textBold: true,
+                        textSize: 12,
+                        borderRadius: 13,
+                        label: 'Add Pictures',
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.add_a_photo,
+                                color: theme.onSurface.withOpacity(.5),
+                                size: 25,
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                'Add Pictures',
+                                style: TextStyle(
+                                    color: theme.onSurface.withOpacity(.5),
+                                    fontSize: 15),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    _imageFiles.isNotEmpty
+                        ? Column(
+                            children: _imageFiles.map((file) {
+                              return Column(
+                                children: [
+                                  Stack(
+                                    // fit: StackFit.expand,
+                                    children: [
+                                      Image.file(
+                                        File(file.path),
+                                        width: double.infinity,
+                                        height: 200,
+                                        fit: BoxFit.cover,
+                                      ),
+                                      Positioned(
+                                          top: 10,
+                                          right: 10,
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                                color: theme.surface,
+                                                shape: BoxShape.circle),
+                                            child: IconButton(
+                                                onPressed: () {},
+                                                icon: Icon(
+                                                  Icons.close,
+                                                  color: theme.error,
+                                                )),
+                                          ))
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
+                                ],
+                              );
+                            }).toList(),
+                          )
+                        : const SizedBox.shrink(),
+                    CustomTextField(
+                      controller: _nameController,
+                      hint: 'Product Name',
+                      height: 55,
+                      width: double.infinity,
+                    ),
+                    const SizedBox(height: 10),
+                    CustomTextField(
+                      controller: _priceController,
+                      hint: 'Price',
+                      height: 55,
+                      width: double.infinity,
+                    ),
+                    const SizedBox(height: 10),
+                    CustomTextField(
+                      controller: _descriptionController,
+                      hint: 'Description',
+                      height: 100,
+                      width: double.infinity,
+                      maxLines: null,
+                      expands: true,
+                    ),
+                    const SizedBox(height: 10),
+                    CustomTextField(
+                      controller: _categoryController,
+                      hint: 'Category',
+                      height: 55,
+                      width: double.infinity,
+                    ),
+                    const SizedBox(height: 10),
+                    CustomTextField(
+                      controller: _sizesController,
+                      hint: 'Sizes (comma separated)',
+                      height: 55,
+                      width: double.infinity,
+                    ),
+                    const SizedBox(height: 10),
+                    CustomTextField(
+                      controller: _colorsController,
+                      hint:
+                          'Colors (format: colorName:colorCode, comma separated)',
+                      height: 55,
+                      width: double.infinity,
                     ),
                     const SizedBox(
-                      height: 10,
+                      height: 20,
                     ),
-                    Text(
-                      'Add Picture',
-                      style: TextStyle(
-                          color: theme.onSurface.withOpacity(.5), fontSize: 15),
+                    CustomTextField(
+                      controller: _tagsController,
+                      hint: 'Tags (format: \#TagName, comma separated)',
+                      height: 55,
+                      width: double.infinity,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+                      decoration: BoxDecoration(
+                        color: theme.tertiary,
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: Text(
+                            'Please upload an image of size chart too regarding the product sizes!',
+                            style: TextStyle(color: theme.onTertiary),
+                          ))
+                        ],
+                      ),
                     )
                   ],
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            CustomTextField(
-              controller: _nameController,
-              hint: 'Product Name',
-              height: 55,
-              width: double.infinity,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: CustomTextField(
-                    controller: _priceController,
-                    hint: 'Size',
-                    height: 55,
-                    width: double.infinity,
+              const SizedBox(height: 20),
+              CustomButton(
+                width: double.infinity,
+                height: 55,
+                primaryColor: theme.primary,
+                textColor: theme.onPrimary,
+                onPressed: () async {
+                  if (_nameController.text.isEmpty ||
+                      _priceController.text.isEmpty ||
+                      _descriptionController.text.isEmpty ||
+                      _categoryController.text.isEmpty ||
+                      _descriptionController.text.isEmpty ||
+                      _sizesController.text.isEmpty ||
+                      _tagsController.text.isEmpty ||
+                      _imageFiles.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                          content:
+                              Text('Fill all the data to add this product!')),
+                    );
+                    return;
+                  }
+
+                  showCustomDialog(context);
+
+                  final productService = FirebaseProductService();
+
+                  // Create a product object
+                  final productData = {
+                    'name': _nameController.text,
+                    'description': _descriptionController.text,
+                    'category': _categoryController.text,
+                    'price': double.tryParse(_priceController.text) ?? 0.0,
+                    'sizes': _sizesController.text
+                        .split(',')
+                        .map((size) => size.trim())
+                        .toList(),
+                    'colors': _colorsController.text
+                        .split(',')
+                        .map((color) => {
+                              'colorName': color.split(':')[0].trim(),
+                              'colorCode': color.split(':')[1].trim()
+                            })
+                        .toList(),
+                    'imageUrls': _imageFiles.isNotEmpty
+                        ? await Future.wait(_imageFiles
+                            .map((file) => productService.uploadImage(file)))
+                        : [],
+                  };
+
+                  // await productService.addProduct(productData);
+
+                  // Clear form
+                  _nameController.clear();
+                  _priceController.clear();
+                  _descriptionController.clear();
+                  _categoryController.clear();
+                  _sizesController.clear();
+                  _colorsController.clear();
+                  setState(() {
+                    _imageFiles.clear(); // Clear the image list
+                  });
+                  closeCustomDialog();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Product added successfully')),
+                  );
+                },
+                textBold: true,
+                textSize: 16,
+                borderRadius: 13,
+                label: 'Add Product',
+                child: Center(
+                  child: Icon(
+                    Icons.save,
+                    color: theme.onPrimary,
                   ),
                 ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Expanded(
-                  child: CustomTextField(
-                    controller: _priceController,
-                    hint: 'Price',
-                    height: 55,
-                    width: double.infinity,
-                  ),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                CustomButton(
-                  width: 70,
-                  height: 55,
-                  primaryColor: theme.primary,
-                  textColor: theme.onPrimary,
-                  onPressed: () {},
-                  textBold: true,
-                  textSize: 12,
-                  borderRadius: 13,
-                  label: 'Add',
-                  child: Center(
-                    child: Icon(
-                      Icons.add,
-                      color: theme.onPrimary,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-
+// Your existing CustomTextField and CustomButton classes remain unchanged
 class CustomTextField extends StatelessWidget {
   const CustomTextField({
     super.key,
@@ -130,11 +291,15 @@ class CustomTextField extends StatelessWidget {
     required this.hint,
     required this.height,
     required this.width,
+    this.maxLines,
+    this.expands = false, // Default to false if not provided
   });
 
   final String hint;
   final double height;
   final double width;
+  final int? maxLines;
+  final bool expands;
   final TextEditingController controller;
 
   @override
@@ -144,13 +309,16 @@ class CustomTextField extends StatelessWidget {
     return Container(
       height: height,
       width: width,
-      padding: EdgeInsets.symmetric(horizontal: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 10),
       decoration: BoxDecoration(
         color: theme.primary.withOpacity(.1),
-        borderRadius: BorderRadius.circular(13),
+        borderRadius: BorderRadius.circular(10),
       ),
       child: Center(
         child: TextField(
+          style: TextStyle(fontSize: 12),
+          maxLines: maxLines ?? (expands ? null : 1), // Adjust maxLines logic
+          expands: expands, // Set expands based on the provided value
           controller: controller,
           decoration: InputDecoration(
             hintText: hint,
@@ -164,15 +332,6 @@ class CustomTextField extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class ProductCategorySection extends StatelessWidget {
-  const ProductCategorySection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
 
--- lib/services/products/firebase/_product_service.dart
+++ lib/services/products/firebase/_product_service.dart
@@ -1,37 +1,86 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
 
 class FirebaseProductService {
   final FirebaseFirestore _firestoreDB = FirebaseFirestore.instance;
   final String _collectionPath = 'categories';
-
-  // Future<void> addSection(String sectionName) async {
-  //   try {
-  //     await _firestoreDB.collection('sections').add(
-  //         {'name': sectionName, 'timeStamp': FieldValue.serverTimestamp()});
-  //   } catch (e) {
-  //     print(e);
-  //   }
-  // }
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final String _productsCollectionPath = 'products';
 
 
-  // Stream<List<Section>> getSections(){
-  //   return _firestoreDB.collection('sections').snapshots().map((snapshot){
-  //     final sections = snapshot.docs.map((doc){
-  //       final data = doc.data();
-  //       return Section.fromFirestore(doc.id, data);
-  //     }).toList();
-  //
-  //     return sections;
-  //   });
-  // }
 
-  // Future<void> removeSection(String id)async{
-  //   try{
-  //     _firestoreDB.collection('sections').doc(id).delete();
-  //   }catch(e){
-  //     print(e);
-  //   }
-  // }
+  Future<String?> uploadImage(XFile imageFile) async {
+    try {
+      final fileName = DateTime.now().millisecondsSinceEpoch.toString();
+      final storageRef = _storage.ref().child('product_images/$fileName');
+      final uploadTask = storageRef.putFile(File(imageFile.path));
+      final snapshot = await uploadTask;
+      final downloadUrl = await snapshot.ref.getDownloadURL();
+      return downloadUrl;
+    } catch (e) {
+      print("Failed to upload image: $e");
+      return null;
+    }
+  }
+
+  // Method to add a new product to Firestore
+  Future<void> addProduct(Map<String, dynamic> productData) async {
+    try {
+      await _firestoreDB.collection(_productsCollectionPath).add(productData);
+      print("Product added successfully.");
+    } catch (e) {
+      print("Failed to add product: $e");
+    }
+  }
+
+  // Method to get all products
+  Stream<List<Product>> getProducts() {
+    return _firestoreDB.collection(_productsCollectionPath).snapshots().map(
+          (snapshot) {
+        final products = snapshot.docs.map(
+              (doc) {
+            final data = doc.data();
+            return Product.fromFirestore(doc.id, data);
+          },
+        ).toList();
+        return products;
+      },
+    );
+  }
+
+  // Method to update a product
+  Future<void> updateProduct(String productId, Map<String, dynamic> productData) async {
+    try {
+      await _firestoreDB.collection(_productsCollectionPath).doc(productId).update(productData);
+      print("Product updated successfully.");
+    } catch (e) {
+      print("Failed to update product: $e");
+    }
+  }
+
+  // Method to delete a product
+  Future<void> deleteProduct(String productId) async {
+    try {
+      await _firestoreDB.collection(_productsCollectionPath).doc(productId).delete();
+      print("Product deleted successfully.");
+    } catch (e) {
+      print("Failed to delete product: $e");
+    }
+  }
+
+  // Method to get the count of products
+  Future<String> getProductCount() async {
+    try {
+      final snapshot = await _firestoreDB.collection(_productsCollectionPath).get();
+      return snapshot.docs.length.toString();
+    } catch (e) {
+      print("Failed to get product count: $e");
+      return '0';
+    }
+  }
 
   Future<void> updateSection(String id, String name) async{
     try{
@@ -176,3 +225,59 @@ class Category {
 //   @override
 //   int get hashCode => id.hashCode ^ name.hashCode ^ timestamp.hashCode;
 // }
+class Product {
+  final String id;
+  final String name;
+  final String description;
+  final String category;
+  final double price;
+  final List<String> sizes;
+  final List<Map<String, String>> colors;
+  final String? imageUrl;
+  final Timestamp timestamp;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.category,
+    required this.price,
+    required this.sizes,
+    required this.colors,
+    this.imageUrl,
+    required this.timestamp,
+  });
+
+  factory Product.fromFirestore(String id, Map<String, dynamic> data) {
+    return Product(
+      id: id,
+      name: data['name'] ?? '',
+      description: data['description'] ?? '',
+      category: data['category'] ?? '',
+      price: (data['price'] as num?)?.toDouble() ?? 0.0,
+      sizes: List<String>.from(data['sizes'] ?? []),
+      colors: List<Map<String, String>>.from(data['colors'] ?? []),
+      imageUrl: data['imageUrl'] as String?,
+      timestamp: data['timestamp'] as Timestamp,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is Product &&
+        other.id == id &&
+        other.name == name &&
+        other.description == description &&
+        other.category == category &&
+        other.price == price &&
+        other.sizes == sizes &&
+        other.colors == colors &&
+        other.imageUrl == imageUrl &&
+        other.timestamp == timestamp;
+  }
+
+  @override
+  int get hashCode => id.hashCode ^ name.hashCode ^ description.hashCode ^ category.hashCode ^
+  price.hashCode ^ sizes.hashCode ^ colors.hashCode ^ imageUrl.hashCode ^ timestamp.hashCode;
+}
